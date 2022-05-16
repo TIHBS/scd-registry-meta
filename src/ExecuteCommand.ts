c90004b2f153672d2f0937ef9813ebd22197b271
GarondEisenfaust <chr.schr@web.de>
2022-05-16 19:08:09 +0200

    returns now the line that the executeCommand method is waiting for

--- src/ExecuteCommand.ts
+++ src/ExecuteCommand.ts
@@ -1,9 +1,22 @@
 import * as child_process from "child_process";
 
+interface Result {
+  process: child_process.ChildProcess;
+  line?: string;
+}
+
+function findByRegex(log: string, regexStr: string): string {
+  const start = log.search(regexStr);
+  const sub = log.substring(start, log.length);
+  const end = sub.search("\n");
+  return sub.substring(0, end);
+}
+
 export async function executeCommand(
   call: string,
+  environment?: {},
   waitFor?: string
-): Promise<child_process.ChildProcess> {
+): Promise<Result> {
   const asArray = call.split(" ");
 
   if (asArray.length == 0) {
@@ -12,16 +25,16 @@ export async function executeCommand(
   const command = asArray.shift();
 
   const textdecoder = new TextDecoder();
-  const child = child_process.spawn(command!, asArray);
+  const child = child_process.spawn(command!, asArray, { env: environment });
   return new Promise((resolve, reject) => {
     child.stdout.addListener("data", (chunk) => {
       const log = textdecoder.decode(chunk);
       console.log(log);
       if (!waitFor) {
-        resolve(child);
+        resolve({ process: child });
       }
       if (log.includes(waitFor!)) {
-        resolve(child);
+        resolve({ process: child, line: findByRegex(log, waitFor!) });
       }
     });
     child.on("close", (code) => {
