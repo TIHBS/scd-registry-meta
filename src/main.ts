09f1a5de1e774b569e986b478b084a1b51e4725c
GarondEisenfaust <chr.schr@web.de>
2022-05-16 19:41:54 +0200

    parses, fetches and sets the contract address and other env variables

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,57 @@ import { executeCommand } from "./ExecuteCommand";
 import * as child_process from "child_process";
 
 const composeFiles = [
-  "external/decentralised-scd-registry/docker-compose.yml",
-  "external/decentralised-scd-registry-frontend/docker-compose.yml",
   "external/external-search-provider/docker-compose.yml",
   "external/webserver-storage/docker-compose.yml",
+  "external/decentralised-scd-registry-frontend/docker-compose.yml",
 ];
 
+const registryComposeFile =
+  "external/decentralised-scd-registry/docker-compose.yml";
+
 const childProcesses: child_process.ChildProcess[] = [];
+const hostIp = "172.17.0.1";
 
 async function main() {
   // Starts swarm
-  const swarm = await executeCommand(
-    "npx bee-factory start 1.5.1",
-    "Welcome to Swarm.... Bzzz Bzzzz Bzzzz"
+  childProcesses.push(
+    (
+      await executeCommand(
+        "npx bee-factory start 1.5.1",
+        undefined,
+        "Welcome to Swarm.... Bzzz Bzzzz Bzzzz"
+      )
+    ).process
   );
-  childProcesses.push(swarm);
+
+  // Starts ganache with the registry contract
+  const toWaitFor = "DEPLOYED REGISTRY CONTRACT AT: ";
+  const registryContractResult = await executeCommand(
+    `docker-compose -f ${registryComposeFile} up`,
+    undefined,
+    toWaitFor
+  );
+  childProcesses.push(registryContractResult.process);
+
+  // Parses the registry address out of the log
+  const registryAddress = registryContractResult.line?.substring(
+    toWaitFor.length,
+    registryContractResult.line.length
+  );
+
+  const environment = {
+    REGISTRY_ADDRESS: registryAddress,
+    ETHEREUM_NETWORK_URL: `http://${hostIp}:8545`,
+    ELASTICSEARCH_URL: `http://${hostIp}:9200`,
+    SWARM_URL: `http://${hostIp}:1633`,
+  };
 
   // Starts the remaining services
   for (const filePath of composeFiles) {
-    const process = await executeCommand(`docker-compose -f ${filePath} up`);
-    childProcesses.push(process);
+    childProcesses.push(
+      (await executeCommand(`docker-compose -f ${filePath} up`, environment))
+        .process
+    );
   }
 
   // Necessary to resolve the promise when the script receives a "SIGINT"
@@ -37,7 +68,7 @@ async function main() {
   return finished;
 }
 
-// Kills all subprocesses
+// Kills all subprocesses on receiving a "SIGINT"
 process.on("SIGINT", function () {
   console.log("Caught interrupt signal");
   childProcesses.forEach((process) => process.kill("SIGINT"));
